8d75f1f6ec7a1bd2098c0dd867ab17742a91b1bf
Sergio {Serchie77} <114149327+Serchie77@users.noreply.github.com>
2024-05-12 19:20:28 +0200

    añadido el puerto para conectar Railway

--- config/conexion.php
+++ config/conexion.php
@@ -6,8 +6,9 @@ try {
     $dbname = getenv('MYSQLDATABASE');
     $user = getenv('MYSQLUSER');
     $password = getenv('MYSQLPASSWORD');
+    $port = getenv('MYSQLPORT'); // Asegúrate de tener esta variable de entorno configurada
     
-    $conexionbd = new PDO("mysql:host=$host; dbname=$dbname;charset=utf8", $user, $password);
+    $conexionbd = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
 } catch (Exception $e) {
     die("Error de CONEXIÓN de la base de datos") . $e->getMessage();
 }
