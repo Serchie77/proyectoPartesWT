0f788cbc4e58e933f04e74ca3d8f2f899fd79258
Sergio {Serchie77} <114149327+Serchie77@users.noreply.github.com>
2024-05-13 14:29:59 +0200

    modificado conexion.php por error de despliegue

--- config/conexion.php
+++ config/conexion.php
@@ -1,14 +1,16 @@
 <?php
-try {
-    // $conexionbd = new PDO("mysql:host=localhost; dbname=partes2024;charset=utf8", "root", "");
 
-    $host = getenv('MYSQLHOST');
-    $dbname = getenv('MYSQLDATABASE');
-    $user = getenv('MYSQLUSER');
-    $password = getenv('MYSQLPASSWORD');
-    $port = getenv('MYSQLPORT'); // Asegúrate de tener esta variable de entorno configurada
-    
-    $conexionbd = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
-} catch (Exception $e) {
-    die("Error de CONEXIÓN de la base de datos") . $e->getMessage();
-}
+$host = getenv('MYSQLHOST');
+$dbname = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
+
+$conexionbd = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
+
+// try {
+//     $conexionbd = new PDO("mysql:host=localhost; dbname=partes2024;charset=utf8", "root", "");
+
+// } catch (Exception $e) {
+//     die("Error de CONEXIÓN de la base de datos") . $e->getMessage();
+// }
